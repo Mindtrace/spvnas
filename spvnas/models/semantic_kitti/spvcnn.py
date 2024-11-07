b69ef469fb988103ed6b894d2e5fc9139b2de231
YasserElHaddar16 <102835910+YasserElHaddar16@users.noreply.github.com>
2024-11-07 21:44:23 +0000

    dynamic in_channel dim

--- spvnas/models/semantic_kitti/spvcnn.py
+++ spvnas/models/semantic_kitti/spvcnn.py
@@ -92,7 +92,7 @@ class SPVCNN(nn.Module):
             self.vres = kwargs['vres']
 
         self.stem = nn.Sequential(
-            spnn.Conv3d(4, cs[0], kernel_size=3, stride=1),
+            spnn.Conv3d(kwargs['num_features'], cs[0], kernel_size=3, stride=1),
             spnn.BatchNorm(cs[0]), spnn.ReLU(True),
             spnn.Conv3d(cs[0], cs[0], kernel_size=3, stride=1),
             spnn.BatchNorm(cs[0]), spnn.ReLU(True))
